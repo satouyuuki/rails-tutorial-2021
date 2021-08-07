dc8ae3283ccfe371dc7a3cde63b97bfdaeb48e73
satoyuki <yuki01377@gmail.com>
2021-08-07 14:24:43 +0900

    途中経過

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
@@ -19,4 +23,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :update, :edit]
 end
